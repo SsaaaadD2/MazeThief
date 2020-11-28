552e65ce3524e275643b1d947c7645939240cd70
Saad Merchant <saadamerchant@gmail.com>
2020-11-28 18:16:10 -0500

    Add file for ai detection (incomplete)

---
+++ Assets/Scripts/AIDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDetection : MonoBehaviour
+{
+    public GameObject detectedText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(detectedText, 
+            new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
+    }
+}
